d63d8c18a90d789792404f16c4e259242b58a7de
Néstor Fabricio Parra <fabri0176@gmail.com>
2020-02-14 22:34:12 -0500

    Django Docker

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -1,4 +1,4 @@
-"""TestPro URL Configuration
+"""firstproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
 from django.urls import path
-from .views import News, Home, Contact 
 
 urlpatterns = [
-    path('', Home, name = 'home'),
-    path('news/', News, name = 'news'),
-    path('contact/', Contact, name = 'contact'),
+    path('admin/', admin.site.urls),
 ]
---
+++ readme.md
@@ -0,0 +1,10 @@
+CREATE NEW PROJECT DJANGO
+sudo docker-compose run web django-admin startproject composeexample .
+example 
+    docker-compose run web django-admin startproject firstproject .
+
+
+
+RUN DOCKER 
+docker-compose up -d;
+
